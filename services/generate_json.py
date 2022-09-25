c6af6798b5fe830f10ed86a2dc6513c03a226fab
Iryna Kruts <i_kruts@ukr.net>
2022-09-25 21:05:50 +0300

    deleted header in wd replaced json file

--- services/generate_json.py
+++ services/generate_json.py
@@ -9,8 +9,8 @@ def generate_json(url) -> str:
     :return: Количество и имена космонавтов в настоящий момент
     """
     response = requests.get(url)
-    with open("../astronauts.json", "w") as f:
+    with open("../data/astronauts.json", "w") as f:
         json.dump(response.json(), f, indent=2)
-    with open("../astronauts.json", "r") as f:
+    with open("../data/astronauts.json", "r") as f:
         data = json.load(f)
     return data["people"]
