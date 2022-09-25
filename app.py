45d1a1e3b842c5a16eed23edb75f1a7f551dc015
Iryna Kruts <i_kruts@ukr.net>
2022-09-25 16:48:01 +0300

    added makefile, flake8 changed app, requirements, create_bd

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
 from application.services import bd_creator
 
-app = Flask('My site')
+app = Flask("My site")
 
 
 @app.route("/")
 def hello():
     return "Hello, world!"
 
-@app.route('/show-all/')
+
+@app.route("/show-all/")
 def show_some_from_bd():
     result = bd_creator()
-    return render_template('show_all.html', result=result, title='showall')
+    return render_template("show_all.html", result=result, title="showall")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- application/services/create_bd.py
+++ application/services/create_bd.py
@@ -4,8 +4,14 @@ from typing import List, Tuple
 from faker import Faker
 
 fake = Faker()
-item_list = [(1, f'{fake.name()}', 2), (2, fake.name(), 33), (3, f'{fake.name()}', 200), (4, fake.name(), 14),
-             (5, f'{fake.name()}', 2), (6, fake.name(), 1222)]
+item_list = [
+    (1, f"{fake.name()}", 2),
+    (2, fake.name(), 33),
+    (3, f"{fake.name()}", 200),
+    (4, fake.name(), 14),
+    (5, f"{fake.name()}", 2),
+    (6, fake.name(), 1222),
+]
 
 
 def bd_creator() -> List:
@@ -14,28 +20,34 @@ def bd_creator() -> List:
     Функция для создания и минимальной работы с БД
     :return: данные из БД [[значение где phone_value>200], первое значение]
     """
-    with sqlite3.connect('phones.sqlite') as bd_connection:
+    with sqlite3.connect("phones.sqlite") as bd_connection:
         cursor = bd_connection.cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS phones(
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS phones(
         phone INTEGER PRIMARY KEY AUTOINCREMENT,
         contact_name TEXT,
         phone_value INTEGER
         )
-        """)
-        cursor.executemany("""
+        """
+        )
+        cursor.executemany(
+            """
            INSERT INTO
               phones(phone, contact_name, phone_value)
            VALUES
         (:phone, :contact_name, :phone_value)
-    """, item_list)
+    """,
+            item_list,
+        )
         read_first_row = cursor.execute("SELECT * FROM phones")
         result_some = read_first_row.fetchone()
         cursor.execute("UPDATE phones SET phone_value=phone_value+500")
         read_all_bd = cursor.execute("SELECT * FROM phones WHERE phone_value > 100")
         result = read_all_bd.fetchall()
         cursor.execute("DELETE FROM phones WHERE phone IN(1,3)")
-        cursor.execute("""
+        cursor.execute(
+            """
                 DROP TABLE phones;
-                """)
+                """
+        )
         return result
-
